b669c3ed5861fbb4284aba08caf581c85beb96d6
Lucas <lucascomaru20@gmail.com>
2022-02-27 22:09:44 -0300

    ex 45 resolvido!

--- ex045.py
+++ ex045.py
@@ -1,9 +1,37 @@
-import random
+from random import randint
+jogo = ('Pedra', 'Papel', 'Tesoura')
+pc = randint(0, 2)
 print("""Suas opções: 
 [1]Pedra
 [2]Papel
 [3]Tesoura """)
-jogada = input('Qual a sua jogada? ')
-jogador = ('Pedra', 'Papel', 'Tesoura')
-pc = random.randint(0,2)
-print(jogador[pc])
+jogada = int(input('Qual a sua jogada? '))
+print(f'O computador escolheu {jogo[pc]}')
+print(f'O jogador escolheu {jogo[jogada]}')
+if pc == 0:  #PC JOGANDO PEDRA
+    if jogada == 0:
+        print('EMPATE! ')
+    elif jogada == 1:
+        print('JOGADOR VENCEu!')
+    elif jogada == 2:
+        print('COMPUTADOR VENCEU! ')
+    else:
+        print('JOGADA INVÁLIDA! ')
+elif pc == 1:
+    if jogada == 0:
+        print('COMPUTADOR VENCEU! ')
+    elif jogada == 1:
+        print('EMPATOU! ')
+    elif jogada == 2:
+        print('JOGADOR VENCEU! ')
+    else:
+        print('JOGADA INVÁLIDA! ')
+elif pc == 2:
+    if jogada == 0:
+        print('JOGADOR VENCE!')
+    elif jogada == 1:
+        print('COMPUTADOR VENCE')
+    elif jogada == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA!')
